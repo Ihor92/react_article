fb5088e7ac92674469958e3c87b69ede2786eecb
Ihor92 <salfetnikov92@gmail.com>
2020-12-08 11:46:32 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.4a97d74912a7774590f27982c2f28dd7.js"
 );
 
 self.addEventListener('message', (event) => {
